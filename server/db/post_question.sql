85a5f4b1b06feeffbc4e78da6f598b039c43414b
ChriswFoster <chriswfoster@gmail.com>
2017-12-20 16:19:28 -0600

    Merged, added userlist content, etc

--- server/db/post_question.sql
+++ server/db/post_question.sql
@@ -1,2 +1,2 @@
-INSERT INTO questions (question,code_block,topic_id, user_id, time)
-VALUES($1,$2,$3,$4, now());
+INSERT INTO questions (question,code_block,topic_id, user_id, answered, time)
+VALUES($1,$2,$3,$4, false, now());
