c2507128d1361cfc5d7ef864773362511681c850
Devin Jackson <Devidu@Devins-MacBook-Pro.local>
2017-12-20 09:50:59 -0600

    fix: the fern back

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -10,7 +10,7 @@ module.exports = {
   postQuestion: (req, res, next) => {
     const dbInstance = req.app.get('db');
     dbInstance
-      .post_question([req.body.text, req.body.code, req.body.topic])
+      .post_question([req.body.text, req.body.code, req.body.topic_id])
       .then(question => res.status(200).json(question))
       .catch(console.log);
   },
