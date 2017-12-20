fc2b7acfb03373b8a3c02d98b42abe7c4ff30afa
Devin Jackson <Devidu@Devins-MacBook-Pro.local>
2017-12-20 17:14:07 -0600

    styles: colors and animations

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -5,14 +5,25 @@ import './Avatar.css';
 class Avatar extends Component {
   constructor(props) {
     super(props);
+    this.state ={
+      hovered: false
+    }
+    this.setButtonHovered = this.setButtonHovered.bind(this)
+  }
+
+  setButtonHovered(val){
+    this.setState({hovered: val})
   }
 
   render() {
     console.log('avatar props', this.props)
     return (
        
-        <div className="user-card">
-
+        <div className="user-card" onMouseEnter={() => this.setButtonHovered(true)} 
+        onMouseLeave={() => this.setButtonHovered(false)}>
+        {this.state.hovered?<div className="user-tooltip">
+          {this.props.user.name}
+        </div>:''}
           <div className="user-card-left" style={{}}>
             <div className="user-avatar shadowed" style={{backgroundImage:`url('${this.props.user.image_url}')`}}/>
           </div>
