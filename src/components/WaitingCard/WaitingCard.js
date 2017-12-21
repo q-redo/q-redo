e4bfe884fa51f8cbd88d6140ed6be8da920725d3
Devin Jackson <Devidu@Devins-MacBook-Pro.local>
2017-12-21 10:32:53 -0600

    styles: add ellipsis and waiting message

--- src/components/WaitingCard/WaitingCard.js
+++ src/components/WaitingCard/WaitingCard.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './WaitingCard.css';
 import axios from 'axios';
 import hourglass from './hourglass.svg';
+import ellipsis from './ellipsis.svg';
 import { relative } from 'path';
 import {connect} from 'react-redux';
 import {toggleAction} from '../../redux/reducer'
@@ -15,9 +16,12 @@ class WaitingCard extends Component {
 
   render() {
     return (
-      <div className="waiting-card-main-container m10 curved shadowed">
+      <div className="waiting-card-main-container m10 flexed curved shadowed">
             <img style={{width: '90px'}} src={hourglass} alt="hourglass spinning"/>
-          
+            <div style={{width: '170px',height: '90px', display: 'inline-flex', justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
+            <span style={{fontSize: '1.5em'}}>STAND BY</span>
+            <img style={{width: '36px', marginBottom: '-16px'}} src={ellipsis} alt="ellipsis"/>
+            </div>
             <i onClick={()=>this.props.toggleAction("action")} class="fa fa-lg fa-times" aria-hidden="true"></i>
       </div>
     );
