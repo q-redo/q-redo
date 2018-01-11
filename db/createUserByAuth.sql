93ed91254fa1c9ffd81d1ea4437ab959c9dc9494
ChriswFoster <chriswfoster@gmail.com>
2018-01-11 10:00:57 -0600

    Fixed new user cohort and campus assignment

--- db/createUserByAuth.sql
+++ db/createUserByAuth.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (auth_id, name, image_url, email, rank) VALUES ($1, $2, $3, $4, 3);
+INSERT INTO users (auth_id, name, image_url, email, rank, campus_id, cohort_id) VALUES ($1, $2, $3, $4, 3, 4, 1);
 SELECT FROM users WHERE auth_id= $1;
