2a0e84e607ee4ec3f5d01d9ab2525fc1b6a7f439
ChriswFoster <chriswfoster@gmail.com>
2018-01-11 18:56:04 -0600

    Charts stuff fixed

--- src/components/AdminView/AdminCards/DeleteQuestions.js
+++ src/components/AdminView/AdminCards/DeleteQuestions.js
@@ -7,11 +7,14 @@ class DeleteQuestions extends Component {
     return (
       <div>
         <div className="big-circle-card m10">
-          <button className="bigCircle  jump shadowed flexed">
-            <i className="fa fa-lg fa-trash" aria-hidden="true" />
-          </button>
           <Link to="/allcharts">
-            <span style={{ marginTop: "10px" }}>Charts</span>
+            <button className="bigCircle  jump shadowed flexed">
+              <i className="fa fa-lg fa-pie-chart" aria-hidden="true" />
+            </button>
+
+            <span style={{ marginTop: "10px", textDecoration: "none" }}>
+              Charts
+            </span>
           </Link>
         </div>
       </div>
